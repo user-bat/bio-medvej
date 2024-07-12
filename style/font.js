088d787797dada48aa22def5b83dc245ce250d81
text <manaeffrom@yandex.ru>
2024-07-12 16:13:32 +0400

    last main

--- style/font.js
+++ style/font.js
@@ -1,13 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const textElement = document.getElementById('typetext');
     const text = "🌙Медвежонок🌙";
-    const span = document.createElement('span');
-    
-    span.textContent='|'
-    // span.style.fontSize = '100%';
-    // span.style.animation='blink 0.5s steps(2, start) infinite';
-    span.classList.add('span');
-    textElement.parentElement.appendChild(span);
+
     let time = 0;
     let index = 0;
     let isAdding = true;
@@ -24,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 if(time < 1){
                     setTimeout(typeWriter, 50);
                 }
-                // setTimeout(typeWriter, 120); 
+              
                 return;
             }
         } else {
@@ -32,18 +26,15 @@ document.addEventListener("DOMContentLoaded", function() {
                 index--;
             } else {
                 isAdding = true;
-                // time++;
-                // if(time < 2){
-                //     setTimeout(typeWriter, 1000);
-                // }
+                
                 setTimeout(typeWriter, 50); 
                 return;
-                // break;
+             
             }
 
         }
         setTimeout(typeWriter, 100); 
-        // break;
+      
     }
 
     typeWriter();
